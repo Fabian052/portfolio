f8522286c811b57059abf681d6ee151735a72297
Fabian Ramos <fabian2103@gmail.com>
2023-07-05 23:06:58 -0500

    responsive

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -13,43 +13,17 @@ const Header = () => {
   ]
 
   return (
-    <header className="w-full fixed top-0 left-0"
+    <header className="w-full fixed top-0 left-0">
 
-    /* className="font-bold sm:text-xl flex align-center justify-between py-5 px-3" */>
-
-
-
-
-      {/*
-      <nav className={`absolute bg-black/90 flex sm:top-4 sm:right-4 gap-3 ${showModal
-        ? "flex gap-3 bg-black/90 py-6 w-full flex-col items-center top-[75px] right-0 max-w-[min(100%,_350px)] text-xl font-medium rounded-md z-100"
-        : "-top-[600px] -right-[600px]"} transition-all`}>
-
-        <Link
-          className="text-center hover:bg-white/30 w-1/2 transition-all py-2 px-2"
-          to="/"
-        >Home</Link>
-
-        <Link
-          className="text-center hover:bg-white/30 w-1/2 transition-all py-2 px-2"
-          to="/work">Work</Link>
-
-        <Link
-          className="text-center hover:bg-white/30 w-1/2 transition-all py-2 px-2"
-          to="/skills">Skills</Link>
-
-        <Link
-          className=" text-center hover:bg-white/30 w-1/2 transition-all py-2 px-2"
-          to="/contact">Contact</Link>
-      </nav>
-
-       */}
-      <button onClick={handleClickShowModal} className="absolute text-2xl right-4 top-4 sm:hidden">
+      <button
+        onClick={handleClickShowModal} className="absolute text-2xl right-4 top-4 sm:hidden">
         <i className={`bx ${showModal ? "bx-x" : "bx-menu-alt-right"}`}></i>
       </button>
-      <section className="sm:flex justify-between items-center py-4 px-7 sm:px-10">
+      <section
+        className="sm:flex justify-between items-center py-4 px-7 sm:px-10">
         <Link className="font-agdasima font-bold text-3xl p-2" to="/">Fabian R.</Link>
-        <ul className={`sm:flex sm:items-center sm:pb-0 pb-12 absolute sm:static sm:z-auto z-[-1] bg-[rgba(24,_24,_35,_1)] sm:bg-black/0 left-0 w-full sm:w-auto pl-12 sm:pl-0 sm:pr-20 transition-all duration-300 ease-in ${showModal ? "top-20" : "top-[-400px]"}`}>
+
+        <ul className={`sm:flex sm:items-center sm:pb-0 pb-7 absolute sm:static sm:z-auto z-[-1] bg-[rgba(24,_24,_35,_1)] sm:bg-black/0 left-0 w-full sm:w-auto pl-12 sm:pl-0 sm:pr-24 transition-all duration-300 ease-in ${showModal ? "top-16" : "top-[-400px]"}`}>
           {
             links.map((link) => (
               <li key={link.name} className="sm:ml-4 text-xl sm:my-0 my-7">
@@ -59,7 +33,7 @@ const Header = () => {
               </li>
             ))
           }
-          <Link to="/contact" className="sm:ml-4 text-xl sm:bg-[#5221E6] sm:absolute sm:right-4 sm:top-0 sm:right-0 py-6 px-6">
+          <Link to="/contact" className="sm:ml-3 text-xl sm:bg-[#5221E6] sm:absolute sm:right-4 sm:top-0 sm:right-0 py-6 sm:px-3">
             Contact
           </Link>
         </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,25 @@
 const Home = () => {
   return (
-    <section /* className="grid justify-end md:relative" */>
+    <section className="" /* className="grid justify-end md:relative" */>
 
       <article className="bg-[rgba(24,_24,_35,_1)]  sm:flex">
         {/* max-w-[480px] mx-auto lg:max-w-[600px] lg:min-h-[530px] xl:max-w-[850px] xl:min-h-[760px] min-[1600px]:max-w-[1000px] min-[1600px]:min-h-[820px] overflow-hidden */}
 
-
-
-
-
-        <div className="relative w-[min(550px,_100%)] h-[400px]" /* className="md:absolute left-0 top-14 xl:top-24" */>
+        {/* <div className="relative sm:absolute w-full sm:w-[50%] h-[45vh]">
           <img
-            className="w-[min(550px,_100%)] absolute object-cover w-full h-full"
+            className="absolute object-contain w-full h-full"
             src="/images/image.png"
             alt="Foto de Fabian Ramos"
           />
-        </div>
-        <div className="flex flex-col md:p-11 lg:p-14 xl:p-16 gap-5 p-3 md:justify-center md:z-10">
+        </div> */}
+        <div className="sm:absolute sm:bg-[rgba(24,_24,_35,_1)] sm:top-0 sm:right-0 w-[70%] z-0 mt-20"
+        /* className="flex flex-col md:p-11 lg:p-14 xl:p-16 gap-5 px-3 py-9 md:justify-center md:z-10" */>
           <h2 className="text-3xl font-semibold sm:text-4xl lg:text-5xl xl:text-7xl min-[1600px]:text-8xl ">
             I'm Fabian Ramos. <br />
             A fronted developer <br />
             <span className="text-gray-400">based in Colombia.</span>
           </h2>
-          <p className="md:text-sm min-[1700px]:text-lg">
+          <p className="">
             I'm probably the most passionate designer you will ever get to work
             with. If you have a great project that needs some amazing skills,
             I'm your guy.
