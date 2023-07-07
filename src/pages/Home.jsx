c1a0ddfe01257f3116ca41e79e24ef4576c2214f
Fabian Ramos <fabian2103@gmail.com>
2023-07-07 17:01:26 -0500

    finish responsive design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 
         <div className="bg-[rgba(24,_24,_35,_1)] px-3 py-8 sm:w-[65vw] sm:h-screen flex flex-col sm:justify-center sm:pl-20 sm:pr-5 xl:pl-28 gap-8 sm:gap-14 md:gap-16 lg:gap-20">
 
-          <h2 className="text-3xl sm:text-[34px] md:text-[2.6rem] lg:text-[58px] xl:text-7xl sm:leading-snug xl:leading-snug font-semibold relative max-w-[550px] md:max-w-[60vw] mx-auto sm:mx-0">
+          <h2 className="text-3xl sm:text-[34px] md:text-[2.6rem] lg:text-[58px] xl:text-7xl sm:leading-snug xl:leading-tight font-semibold relative max-w-[550px] md:max-w-[60vw] mx-auto sm:mx-0">
             I'm Fabian Ramos. <br />
             A fronted developer <br />
             <span className="text-gray-400">based in Colombia.</span>
